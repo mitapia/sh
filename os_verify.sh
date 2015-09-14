60912ede180b717129dc781d62320ca1207a70d1
mitapia <mitapia@gmail.com>
2015-09-14 02:04:43 -0500

    Update os_verify.sh

--- os_verify.sh
+++ os_verify.sh
@@ -1,1 +1,15 @@
 #!/bin/sh
+if [ $(uname -s) == "Linux" ] ; then 
+	echo "enviroment runnign is Linux"
+	if [ -e /etc/redhat-release ] ; then
+		cat /etc/redhat-release;
+	fi
+
+	if [ -e /etc/lsb-release ]; then
+	 	cat /etc/lsb-release;
+	fi
+fi 
+
+if [ $(uname -s) == "FreeBSD" ] ; then 
+	echo "enviroment runnign is FreeBSD"
+fi 
