86f7ec1f0e8e43444e0d790c26f84346c8abe74b
mitapia <mitapia@gmail.com>
2015-09-16 07:42:39 -0500

    Update os_verify.sh

--- os_verify.sh
+++ os_verify.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
-if [ $(uname -s) == "Linux" ] ; then 
-	echo "enviroment runnign is Linux"
-	if [ -e /etc/redhat-release ] ; then
-		cat /etc/redhat-release;
+os=$(uname -s);
+
+if [ $os == "Linux" ]; then
+	# if XenServer or ESX then dont run script
+	# missiong test ESX
+	if [[ -e /etc/redhat-release ]]; then
+		#statements
 	fi
 
-	if [ -e /etc/lsb-release ]; then
-	 	cat /etc/lsb-release;
-	fi
-fi 
+	# if QuantaStore, raid_verify
+	# missing test server
 
-if [ $(uname -s) == "FreeBSD" ] ; then 
-	echo "enviroment runnign is FreeBSD"
-fi 
+	# if vyatta - not working yet
+	if [[ $(show version | grep -i vyatta | wc -l) -gt 0 ]]; then
+		#statements
+	fi
+
+	# if CentOS or RHEL, then check for ver.
+	# all other
+
+
+else
+	echo "This OS has not been properly tested.  Please email the following results to mitapia@softlayer.com:";
+	uname -a;
+fi
