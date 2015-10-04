5716154a9ead7fcf275d5877351b15b94f5d2444
mitapia <mitapia@softlayer.com>
2015-10-04 02:18:33 -0500

    Delete download_test.sh

--- download_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# random command to test the dowload and running of script using screen
-hostname; 
-echo $SHELL;
-if [ -f /etc/redhat-release ]; then
-	echo "yum here";
-else
-	echo "apt-get here";
-fi
